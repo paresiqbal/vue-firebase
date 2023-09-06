74e942d3a7a1b6af4c1d4f9537f344189e790b60
paresiqbal <thecrowdel@gmail.com>
2023-09-06 22:35:34 +0700

    Add news form

---
+++ src/views/AddNews.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create News</h1>
+    <form>
+      <label for="title" aria-label="title">Title</label>
+      <input type="text" name="title" required />
+      <label for="body" aria-label="body">Body</label>
+      <input type="text" name="body" required />
+      <button type="submit">Post news</button>
+    </form>
+
+    <form>
+      <label for="id">Document id:</label>
+      <input type="text" name="id" required />
+      <button>Delete news</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
