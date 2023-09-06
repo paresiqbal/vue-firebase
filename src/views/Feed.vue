e4b543b9dc61445ef02d8b96b1f7d8463340a62f
paresiqbal <thecrowdel@gmail.com>
2023-09-06 23:10:21 +0700

    Change everything to the start

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,32 +1,25 @@
 <template>
-  <div class="flex flex-col justify-center items-center">
+  <div>
     <h1 class="text-center">FEED</h1>
-    <a href="/addnews" class="px-4 py-2 bg-cyan-500 rounded-md text-white"
-      >Make a post</a
-    >
-
-    <!-- Display the news -->
-    <div v-for="itemBerita in berita" :key="itemBerita.id">
-      <h2>{{ itemBerita.title }}</h2>
-      <p>{{ itemBerita.body }}</p>
-    </div>
   </div>
 </template>
 
 <script setup>
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../main";
-import { ref, onMounted } from "vue";
 
 const colRef = collection(db, "berita");
-const berita = ref([]);
 
-onMounted(async () => {
-  try {
-    const snapshot = await getDocs(colRef);
-    berita.value = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+getDocs(colRef)
+  .then((snapshot) => {
+    let berita = [];
+
+    snapshot.docs.forEach((doc) => {
+      berita.push({ ...doc.data(), id: doc.id });
+      console.log(berita);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 </script>
