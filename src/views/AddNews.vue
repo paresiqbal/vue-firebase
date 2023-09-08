b23e1d72cbbe4922f00586847040bd5620f27c87
paresiqbal <thecrowdel@gmail.com>
2023-09-08 21:25:56 +0700

    Add something

--- src/views/AddNews.vue
+++ src/views/AddNews.vue
@@ -43,42 +43,40 @@
 </template>
 
 <script setup>
-// Vue
 import { ref } from "vue";
-
-// Firebase
 import { collection, addDoc } from "firebase/firestore";
-import { storage } from "../main"; // Import Firebase storage
+import { storage, db } from "../main"; // Import Firebase storage
 
-// Create reactive data properties for the form fields
 const title = ref("");
 const body = ref("");
-const imageFile = ref(null); // To store the selected image file
+const imageFile = ref(null);
 
-// Method to handle the file input change for image upload
 const handleImageUpload = (event) => {
-  // Get the selected image file
   imageFile.value = event.target.files[0];
 };
 
-// Method to handle the form submission
 const addNews = async () => {
   try {
-    // Upload the image file to Firebase Storage
+    let imageUrl = ""; // Initialize imageUrl
+
+    // Check if an image file is selected
     if (imageFile.value) {
-      const storageRef = storage.ref(); // Reference to the root of Firebase Storage
+      const storageRef = storage.ref();
       const imageFileName = Date.now() + "-" + imageFile.value.name;
       const imageRef = storageRef.child(imageFileName);
 
-      await imageRef.put(imageFile.value);
-      const imageUrl = await imageRef.getDownloadURL(); // Get the URL of the uploaded image
+      // Upload the image file to Firebase Storage
+      const snapshot = await imageRef.put(imageFile.value);
+
+      // Get the download URL of the uploaded image
+      imageUrl = await snapshot.ref.getDownloadURL();
     }
 
     // Create a new document in the "berita" collection with the image URL
     const docRef = await addDoc(collection(db, "berita"), {
       title: title.value,
       body: body.value,
-      imageUrl: imageUrl, // Add the image URL to the news document
+      imageUrl: imageUrl,
     });
 
     // Clear the form fields after successfully adding the news
