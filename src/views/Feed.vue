797d837cdf50250d2643f1eda6d3febdb29221d2
paresiqbal <thecrowdel@gmail.com>
2023-09-09 12:57:13 +0700

    Adding button for edit

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -9,34 +9,41 @@
       <a @click="showNewsDetail(newsItem.id)">
         <h2 class="text-xl font-bold">{{ newsItem.title }}</h2>
       </a>
+      <!-- Add an "Edit" button to edit the news item -->
+      <button @click="editNews(newsItem.id)" class="text-sm text-blue-500">
+        Edit
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
-// Vue
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-
-// Firebase
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../main";
 
 const colRef = collection(db, "berita");
-const berita = ref([]); // Create a reactive data property to store the news
+const berita = ref([]);
 const router = useRouter();
 
-getDocs(colRef)
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      berita.value.push({ ...doc.data(), id: doc.id });
-    });
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+const fetchNews = async () => {
+  try {
+    const snapshot = await getDocs(colRef);
+    berita.value = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  } catch (error) {
+    console.error("Error fetching news: ", error);
+  }
+};
 
 const showNewsDetail = (newsItemId) => {
   router.push({ name: "news-detail", params: { id: newsItemId } });
 };
+
+// Function to route to the AddNews page for editing
+const editNews = (newsItemId) => {
+  router.push({ name: "AddNews", params: { id: newsItemId } });
+};
+
+fetchNews();
 </script>
