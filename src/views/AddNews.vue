dbd0adab18f0e7681a68d7e2f4b3f937873440f7
paresiqbal <thecrowdel@gmail.com>
2023-09-11 23:26:45 +0700

    asdasd

--- src/views/AddNews.vue
+++ src/views/AddNews.vue
@@ -1,41 +1,45 @@
 <template>
   <div class="flex flex-col py-8 justify-center items-center">
-    <h1 class="text-4xl text-amber-500 font-bold py-4">Add News</h1>
+    <h1 class="text-4xl text-amber-500 font-bold py-4">Upload Berita</h1>
     <form
       class="flex flex-col gap-4 p-4 bg-slate-800 rounded-md text-white"
       @submit.prevent="addNews"
     >
-      <label for="title">Title</label>
-      <input
-        class="text-sm text-gray-900 outline-none rounded-md py-1 pl-1"
-        type="text"
-        name="title"
-        v-model="title"
-        placeholder="Insert your title"
-      />
-      <label for="body">Body</label>
-      <textarea
-        class="text-sm text-gray-900 outline-none rounded-md py-1 pl-1"
-        name="body"
-        id="body"
-        v-model="body"
-        cols="30"
-        rows="10"
-      ></textarea>
-      <label for="image">Image</label>
-      <input
-        class="text-sm text-gray-900 outline-none rounded-md py-1 pl-1"
-        type="file"
-        accept="image/*"
-        id="image"
-        ref="imageUpload"
-        @change="handleImageUpload"
-      />
+      <div class="flex flex-col">
+        <label for="title">Title</label>
+        <input
+          class="text-sm text-gray-900 outline-none rounded-md py-1 pl-1"
+          type="text"
+          name="title"
+          v-model="title"
+          placeholder="Insert your title"
+        />
+        <label for="body">Body</label>
+        <textarea
+          class="text-sm text-gray-900 outline-none rounded-md py-1 pl-1"
+          name="body"
+          id="body"
+          v-model="body"
+          cols="30"
+          rows="10"
+        ></textarea>
+      </div>
+      <div>
+        <label for="image">Image</label>
+        <input
+          class="text-sm text-gray-900 outline-none rounded-md py-1 pl-1"
+          type="file"
+          accept="image/*"
+          id="image"
+          ref="imageUpload"
+          @change="handleImageUpload"
+        />
+      </div>
       <button
         class="text-gray-900 px-4 py-2 bg-white rounded-md font-bold"
         type="submit"
       >
-        Upload News
+        Upload Berita
       </button>
     </form>
   </div>
