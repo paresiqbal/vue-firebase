6304db9c3a007841cc0a128ff66ac807f5d465bb
paresiqbal <thecrowdel@gmail.com>
2023-09-12 00:09:26 +0700

    dd Pmb

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Carousel from "../components/Carousel.vue";
+import Pmb from "../components/Pmb.vue";
 
 // Components
 </script>
 
 <template>
   <Carousel />
+  <Pmb />
 </template>
