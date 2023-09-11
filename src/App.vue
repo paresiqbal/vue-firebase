7d4a73d5dd91b09f4f480ee935a9a68f04dfe1e6
paresiqbal <thecrowdel@gmail.com>
2023-09-11 19:47:54 +0700

    Add daisyui

--- src/App.vue
+++ src/App.vue
@@ -29,18 +29,34 @@ const handleSignOut = () => {
 </script>
 
 <template>
-  <div class="py-4 px-36 text-white bg-green-600">
+  <div class="py-2 px-36 text-white bg-green-600">
     <a href="/" class="font-bold text-lg">S1-Ilmu Komputer</a>
   </div>
-  <nav
-    class="flex gap-8 justify-center py-6 bg-green-500 text-white font-5xl font-bold"
-  >
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/feed">Feed</RouterLink>
-    <RouterLink to="/addnews" v-if="isLogged">Add News</RouterLink>
-    <RouterLink to="/register" v-if="!isLogged">Register</RouterLink>
-    <RouterLink to="/signin" v-if="!isLogged">Sign In</RouterLink>
-    <button @click="handleSignOut" v-if="isLogged">Sign Out</button>
+
+  <nav>
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+        <span class="flex justify-center items-center font-bold text-xl"
+          ><img src="./assets/upp.png" alt="logo" class="w-12" /><a href="/"
+            >Universitas Pat Petulai</a
+          ></span
+        >
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><a>Link</a></li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul class="p-2 bg-base-100">
+                <li><a>Link 1</a></li>
+                <li><a>Link 2</a></li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
   <RouterView />
 </template>
