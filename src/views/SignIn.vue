a8393304e5236f6daa2cb32538fdd6bbab646355
paresiqbal <thecrowdel@gmail.com>
2023-09-11 23:09:39 +0700

    Update ui for sign

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,38 +1,56 @@
 <template>
-  <div class="flex flex-col items-center gap-2 justify-center py-8">
+  <section
+    class="mx-auto px-8 overflow-hidden max-w-lg py-4"
+    aria-labelledby="signin"
+  >
     <h1 class="text-center text-4xl font-bold text-amber-500">Sign In</h1>
-    <p>
-      <input
-        class="ring-1 ring-cyan-500 rounded-sm"
-        type="email"
-        name="email"
-        placeholder="Your Email"
-        v-model="email"
-      />
-    </p>
-    <p>
-      <input
-        class="ring-1 ring-cyan-500 rounded-sm"
-        type="password"
-        name="password"
-        placeholder="Your Password"
-        v-model="password"
-      />
-    </p>
-    <p class="text-red-500" v-if="errMsg">{{ errMsg }}</p>
-    <button
-      @click="signIn"
-      class="mt-4 py-2 px-8 bg-cyan-500 active:bg-cyan-600 rounded-md text-white font-bold"
-    >
-      Sign In
-    </button>
-    <button
-      @click="signWithGoogle"
-      class="mt-4 py-2 px-2 bg-cyan-500 active:bg-cyan-600 rounded-md text-white font-bold"
-    >
-      Sign In with Google
-    </button>
-  </div>
+    <div class="border-2 border-neutral-800 rounded-lg grid gap-4 my-12 p-6">
+      <div>
+        <label
+          for="email"
+          class="text-neutral-500 text-sm font-bold uppercase pb-4"
+          >Your Email:</label
+        >
+        <input
+          class="w-full border-4 border-amber-400 rounded-md focus:ring-4 focus:border-amber-400 focus:ring-amber-400 focus:outline-none"
+          type="email"
+          name="email"
+          placeholder="Email"
+          v-model="email"
+        />
+      </div>
+      <div>
+        <label
+          for="password"
+          class="text-neutral-500 text-sm font-bold uppercase pb-4"
+          >Your Password:</label
+        >
+        <input
+          class="w-full border-4 border-amber-400 rounded-md focus:ring-4 focus:border-amber-400 focus:ring-amber-400 focus:outline-none"
+          type="password"
+          name="password"
+          placeholder="Password"
+          v-model="password"
+        />
+      </div>
+      <p class="text-red-500" v-if="errMsg">{{ errMsg }}</p>
+      <div class="flex flex-col">
+        <button
+          @click="signIn"
+          class="mt-4 py-2 px-8 bg-amber-500 active:bg-amber-400 rounded-md text-white font-bold"
+        >
+          Sign In
+        </button>
+        <button
+          @click="signWithGoogle"
+          class="mt-4 py-2 px-2 rounded-md text-slate-900 border-2 border-slate-900 font-bold flex justify-center items-center gap-2 active:bg-slate-100"
+        >
+          <img src="../assets/google.png" alt="google" class="w-6" />
+          Sign In with Google
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
