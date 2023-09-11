8698f2fd8701ccd5c7e72cff7bdcad416dc61979
paresiqbal <thecrowdel@gmail.com>
2023-09-11 20:02:29 +0700

    asdasd

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,10 +1,7 @@
-<script setup>
-import Tes from "../components/Tes.vue";
-</script>
+<script setup></script>
 
 <template>
   <div>
     <h1 class="text-center">HOME</h1>
-    <Tes />
   </div>
 </template>
