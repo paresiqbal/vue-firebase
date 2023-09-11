2260d1e36d1c733d4a3880a104012300a91e96e4
paresiqbal <thecrowdel@gmail.com>
2023-09-12 00:14:34 +0700

    Add gallery

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -2,12 +2,12 @@
 import Carousel from "../components/Carousel.vue";
 import Pmb from "../components/Pmb.vue";
 import Konsentrasi from "../components/Konsentrasi.vue";
-
-// Components
+import Gallery from "../components/Gallery.vue";
 </script>
 
 <template>
   <Carousel />
   <Pmb />
   <Konsentrasi />
+  <Gallery />
 </template>
